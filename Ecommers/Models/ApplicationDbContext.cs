24fc4b066947d7174d72c73ce225d4ce6aa8efa4
Aria <calixto.hdz8malu@gmail.com>
2024-12-03 17:05:43 -0600

    feat: se actualizo todo lo de productos

---
+++ Ecommers/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ecommers.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Constructor para recibir las opciones de configuración
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSet para representar la tabla Productos
+        public DbSet<Product> Productos { get; set; }
+    }
+}
--- Ecommers/Models/Product.cs
+++ Ecommers/Models/Product.cs
@@ -1,12 +1,24 @@
-﻿namespace Ecommers.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommers.Models
 {
     public class Product
     {
+        [Key] // Esto indica que ProductoID es la clave primaria
         public int ProductoID { get; set; }
+
+        [Required]
         public string Nombre { get; set; }
+
         public string Descripcion { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
         public decimal Precio { get; set; }
+
+        [Required]
         public int Stock { get; set; }
+
         public string ImagenURL { get; set; }
     }
 }
