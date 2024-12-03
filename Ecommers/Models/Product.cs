01819c7c0fec53376b7535ef9030a6ae52ba11cd
Aria <calixto.hdz8malu@gmail.com>
2024-12-03 16:09:18 -0600

    feat: se crea el modelo producto

---
+++ Ecommers/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Ecommers.Models
+{
+    public class Product
+    {
+        public int ProductoID { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+        public string ImagenURL { get; set; }
+    }
+}
