3c0cd059958c655dc989f2f8f1a8ae2db545dc92
Aria <calixto.hdz8malu@gmail.com>
2024-12-06 14:26:40 -0600

    feat: Create controller, view and model CartItem

---
+++ Ecommers/Models/CartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Ecommers.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice => Price * Quantity; // Calcula el precio total por producto
+    }
+}
